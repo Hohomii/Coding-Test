03dcffa6f184115571d7db20603974b884095a74
Hohomii <127713815+Hohomii@users.noreply.github.com>
2023-03-15 09:43:41 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -3,7 +3,7 @@ class Solution {
         int lamp = n*12000;
         int drink = k*2000;
         if (n >= 10) {
-            int count = (int)Math.floor((double)n/10);
+            int count = n / 10;
             drink = drink - (count*2000);
         }
         int answer = lamp + drink;
