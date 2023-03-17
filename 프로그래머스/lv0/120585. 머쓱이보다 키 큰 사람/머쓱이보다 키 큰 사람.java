b28c7fe4ca8f302fe9a4d855e92dbda0f40a9b06
Hohomii <127713815+Hohomii@users.noreply.github.com>
2023-03-17 15:24:50 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int[] array, int height) {
         int answer = 0;
-        for (int i : array) {
-            if (i > height) {
-                answer++;
-            } 
-        } 
         return answer;
     }
 }
