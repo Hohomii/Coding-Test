bb1d5c17977c960173a73f00ab95a8597a4e7344
Hohomii <127713815+Hohomii@users.noreply.github.com>
2023-04-06 23:57:05 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char c1 = my_string.charAt(num1);
+        char c2 = my_string.charAt(num2);
+        char[] arr = my_string.toCharArray();
+        arr[num1] = c2;
+        arr[num2] = c1;
+        return String.valueOf(arr);
+    }
+}
