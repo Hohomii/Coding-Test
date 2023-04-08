e11d49146b6f1e3b7b593b1bc3df3928fe03227f
Hohomii <127713815+Hohomii@users.noreply.github.com>
2023-04-08 11:10:10 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 1.87 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,26 @@
+// 단어를 공백 기준 자른 후 하나하나를  [try, hello, world]
+// 홀수번째 인덱스만 대문자로 치환
+// 인덱스를 생성해서 활용!
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
+
+        int idx = 0;
+        for(int i=0; i<str.length; i++) {
+            if(str[i].equals(" ")) {
+                idx = 0;
+            } else if(idx % 2 == 0) {
+                str[i] = str[i].toUpperCase();
+                idx++;
+            } else if(idx % 2 != 0) {
+                str[i] = str[i].toLowerCase();
+                idx++;
+            }
+            answer += str[i];
+        }
+
+        return answer;
+    }
+}
