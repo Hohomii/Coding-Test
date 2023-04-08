6713f0ada262ae17d198eadcf690e9932aa1cd16
Hohomii <127713815+Hohomii@users.noreply.github.com>
2023-04-08 12:25:56 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,44 @@
+//n을 쪼갠다 => n을 10으로 나눈 나머지들을 계속 만든다. 
+//일반 for문 쓸 수 없음 => long index 불가
+
+// 계속 long타입 때문에 고전했는데.. 
+// string으로 바꿔서 하면 되는 거였다..
+class Solution {
+    public int[] solution(long n) {
+        String num = Long.toString(n);
+        int[] answer = new int[num.length()];
+        int idx = 0;
+        while(n > 0) {
+            answer[idx] = (int)(n % 10);
+            n = n / 10;
+            idx++;
+        }
+        return answer;
+        
+        
+        
+        
+    }
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         int[] answer = new int[(int)(Math.log10(n)+1)]; //n의 길이의 배열 선언
+//         int idx = 0;
+//         while(n > 0) {
+//             answer[idx] = (int)n % 10;
+//             n = n / 10;
+//             idx++;
+//             if (idx == n)
+//                 break;
+//         }
+//         return answer;
+//     }
+// }
