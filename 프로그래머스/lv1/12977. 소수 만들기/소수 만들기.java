508396055ec587d99178389ebc37d8bdf630fa19
Hohomii <127713815+Hohomii@users.noreply.github.com>
2023-04-12 21:37:25 +0900

    [level 1] Title: 소수 만들기, Time: 0.39 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,32 +1,26 @@
-// 3개를 더했을 때 소수가 되는 경우의 수
-// 012, 013, 123
-import java.util.*;
-class Solution {
-    public int solution(int[] nums) {
-        int answer = 0;
-        Arrays.sort(nums); // 정렬하여 중복 계산을 피함.
-        for (int i=0; i < nums.length-2; i++) {
-            for (int j=i+1; j < nums.length-1; j++) {
-                for (int k=j+1; k < nums.length; k++) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if (isPrime(sum)) {
-                        answer++;
-                    }
+public int solution(int[] nums) {
+    int answer = 0;
+
+    for(int i = 0; i < nums.length-2; i++) {
+
+        for(int j = i+1; j < nums.length-1; j++) {
+
+            for(int k = j+1; k < nums.length; k++) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(isPrime(sum)) {
+                    answer += 1;
                 }
             }
         }
-        return answer;
     }
-    
-    private boolean isPrime(int n) {
-        if (n < 2) {
+    return answer;
+}
+
+private boolean isPrime(int number) {
+    for(int i = 2; i <= number/2; i++) {
+        if(number % i == 0) {
             return false;
         }
-        for (int i=2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-         return true;
     }
+    return true;
 }
