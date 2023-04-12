da702032cdfbafb9c5b6aa25a477624e03834c0d
Hohomii <127713815+Hohomii@users.noreply.github.com>
2023-04-12 22:46:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.29 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,20 +1,32 @@
-// s의 숫자는 숫자로 들어가게.
-// 단어와 숫자를 if문 처리 : string.contains() 있으면 해당 숫자를 넣는다.
-// 어레이리스트에 추가하는 식으로 한 담에 숫자로 바꿀까보다.
 import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        s = s.replaceAll("zero", "0");
-        s = s.replaceAll("one", "1");
-        s = s.replaceAll("two", "2");
-        s = s.replaceAll("three", "3");
-        s = s.replaceAll("four", "4");
-        s = s.replaceAll("five", "5");
-        s = s.replaceAll("six", "6");
-        s = s.replaceAll("seven", "7");
-        s = s.replaceAll("eight", "8");
-        s = s.replaceAll("nine", "9");
+        String[] num = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] alphabet = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        
+        for (int i=0; i < num.length; i++) {
+            s = s.replaceAll(alphabet[i], num[i]);
+        }
         return Integer.parseInt(s);
     }
 }
+
+
+// import java.util.*;
+// class Solution {
+//     public int solution(String s) {
+//         int answer = 0;
+//         s = s.replaceAll("zero", "0");
+//         s = s.replaceAll("one", "1");
+//         s = s.replaceAll("two", "2");
+//         s = s.replaceAll("three", "3");
+//         s = s.replaceAll("four", "4");
+//         s = s.replaceAll("five", "5");
+//         s = s.replaceAll("six", "6");
+//         s = s.replaceAll("seven", "7");
+//         s = s.replaceAll("eight", "8");
+//         s = s.replaceAll("nine", "9");
+//         return Integer.parseInt(s);
+//     }
+// }
